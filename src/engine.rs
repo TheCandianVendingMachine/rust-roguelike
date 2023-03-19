ff92cb9114369e15e5c52fa52aaefbeb6343bad5
Bailey <baileydanyluk@gmail.com>
2023-03-18 18:21:36 -0600

    Add engine event handler to manage events relevant to the engine

--- src/engine.rs
+++ src/engine.rs
@@ -19,6 +19,37 @@
 use crate::input::Input;
 use crate::fence::FenceRC;
 use std::sync::mpsc;
+use std::rc::Rc;
+
+enum EngineEvent {
+    Stop
+}
+
+struct EngineEventHandler {
+    event_queue: mpsc::Receiver<EngineEvent>,
+    sender_base: Rc<mpsc::Sender<EngineEvent>>
+}
+
+impl EngineEventHandler {
+    pub fn new() -> EngineEventHandler {
+        let (send, recv) = mpsc::channel();
+        EngineEventHandler {
+            event_queue: recv,
+            sender_base: Rc::new(send)
+        }
+    }
+
+    pub fn get_publisher(&mut self) -> Rc<mpsc::Sender<EngineEvent>> {
+        self.sender_base.clone()
+    }
+
+    pub fn pop(&mut self) -> Option<EngineEvent> {
+        match self.event_queue.try_recv() {
+            Ok(event) => Some(event),
+            Err(_) => None
+        }
+    }
+}
 
 /// The way the game is initialised and ran.
 ///
@@ -28,33 +59,58 @@ use std::sync::mpsc;
 pub struct Engine {
     render_fence: Option<FenceRC>,
     input_queue: mpsc::Receiver<Input>,
+    engine_event_handler: EngineEventHandler,
     running: bool
 }
 
 impl Engine {
     pub fn new(input_queue: mpsc::Receiver<Input>, render_fence: Option<FenceRC>) -> Engine {
+        let engine_event_handler = EngineEventHandler::new();
+
         Engine {
             render_fence,
             input_queue,
+            engine_event_handler,
             running: true
+        }
+    }
+
+    fn handle_input(&mut self) {
+        loop {
+            let input = match self.input_queue.try_recv() {
+                Ok(input) => input,
+                _ => { break }
+            };
+
+            // Handle input
+        }
+    }
+
+    fn handle_engine_events(&mut self) {
+        while let Some(event) = self.engine_event_handler.pop() {
+            match event {
+                EngineEvent::Stop => self.running = false,
+            }
+        }
+    }
+
+    fn update_state(&mut self) {
+
+    }
+
+    fn sync(&self) {
+        if let Some(render_fence) = &self.render_fence {
+            render_fence.0.close().pass();
         }
     }
 
     /// Simulate one tick of the game state
     pub fn tick(&mut self) {
         while self.running {
-            loop {
-                let input = match self.input_queue.try_recv() {
-                    Ok(input) => input,
-                    _ => { break }
-                };
-
-                // Handle input
-            }
-
-            if let Some(render_fence) = &self.render_fence {
-                render_fence.0.close().pass();
-            }
+            self.handle_input();
+            self.update_state();
+            self.sync();
+            self.handle_engine_events();
         }
     }
 }
